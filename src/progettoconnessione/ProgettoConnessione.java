1501c0b4716e7f411e6bf4b024b8b1f4d52d5813
Studenti <Studenti@DESKTOP-J4TRQKA>
2018-02-01 12:13:37 +0100

    scaricato progetto

--- src/progettoconnessione/ProgettoConnessione.java
+++ src/progettoconnessione/ProgettoConnessione.java
@@ -15,7 +15,8 @@ public class ProgettoConnessione {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //importato
     }
     
 }
